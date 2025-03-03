f460b999400dfda6e4569723d76c2435e8f3b82d
Danil Ushakov <danilusakov16@gmail.com>
2025-03-03 11:40:00 +0500

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# WPF Application: Resume & Number Guessing Game
+
+## Description
+
+This is a simple WPF application that includes two features:
+
+1. **Show Resume** - Displays a brief resume using a sequence of MessageBox dialogs.
+2. **Guess Number Game** - The program attempts to guess a number between 1 and 2000 that the user has thought of, using MessageBox dialogs.
+
+## Features
+
+### Show Resume
+
+- Displays a resume in multiple MessageBox dialogs.
+- The last MessageBox title shows the average number of characters per resume line.
+
+### Guess Number Game
+
+- The user thinks of a number between 1 and 2000.
+- The program makes random guesses until it finds the correct number.
+- The number of attempts is displayed when the correct number is guessed.
+- After a successful guess, the user can choose to play again.
+
+## Installation & Usage
+
+1. Clone or download the repository.
+2. Open the project in Visual Studio.
+3. Build and run the application.
+4. Click the **Show Resume** button to view the resume.
+5. Click the **Guess Number** button to start the number guessing game.
+
+## Technologies Used
+
+- C#
+- WPF (Windows Presentation Foundation)
+- XAML
+
+## UI Layout
+
+- The application contains two buttons:
+  - "Show Resume" on the left
+  - "Guess Number" on the right
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowResume_Click(object sender, RoutedEventArgs e)
+        {
+            string[] resume = new string[]
+            {
+                "Name: Danil",
+                "Age: 18",
+                "Country: Russia",
+                "Skills: C++, C#, Dart Flutter, PostgreSQL",
+                "Hobbies: Programming, Reading, Gaming"
+            };
+
+            int totalChars = resume.Sum(k => k.Length);
+            int avgChars = totalChars / resume.Length;
+
+            for (int i = 0; i < resume.Length; i++)
+            {
+                string title = i == resume.Length - 1 ? $"Avg Chars: {avgChars}" : $"Line {i + 1}";
+                MessageBox.Show(resume[i], title, MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void GuessNumber_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            while (true)
+            {
+                int min = 1, max = 2000, attempts = 0;
+                MessageBox.Show("Think of a number from 1 to 2000, and I will try to guess it!", "Game Start", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                while (min <= max)
+                {
+                    int guess = random.Next(min, max + 1);
+                    attempts++;
+
+                    MessageBoxResult response = MessageBox.Show($"Is your number {guess}?", "Attempt #" + attempts, MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+
+                    if (response == MessageBoxResult.Yes)
+                    {
+                        MessageBox.Show($"I guessed your number {guess} in {attempts} attempts!", "Victory!", MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Game interrupted.", "Exit", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        return;
+                    }
+                }
+
+                MessageBoxResult playAgain = MessageBox.Show("Do you want to play again?", "Game Over", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (playAgain == MessageBoxResult.No)
+                    break;
+            }
+        }
+
+    }
+}
